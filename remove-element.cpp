2df5d6854b7e4ef2bfef86625f41ccf0ab6f720d
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-02-21 09:22:47 -0800

    27-Remove Element problem solved

---
+++ remove-element.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            if (nums[left] != val) {
+                ++left;
+            } else {
+                swap(nums[left],  nums[--right]);
+            }
+        }
+        return right;
+    }
+};
