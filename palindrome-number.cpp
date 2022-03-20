e44a2855f7624935910da2da5727e07de2ea6e9e
Abdullah Al Mahi <abdullah.mahi@selise.ch>
2022-03-20 21:44:12 +0600

    9.Palindrome-number problem solved

---
+++ palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int reversed = 0;
+        while (temp != 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return reversed == x;
+    }
+};
