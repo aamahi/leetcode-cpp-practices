38e275401ecd2d6ba98248c2cbce319c291a60f8
Abdullah Al Mahi <abdullah.mahi@selise.ch>
2022-06-09 18:24:21 +0600

    Contans duplicate solved

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> nums_set(nums.begin(), nums.end());
+        return nums_set.size() != nums.size();
+    }
+};
+
+class Solution2 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return unique(nums.begin(), nums.end()) != nums.end();
+    }
+};
