c371c2198a648c3ed11a69fa3fc6f8cfebc9643c
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-08-23 23:35:54 +0600

    can-make-arithmetic-progression-from-sequence

---
+++ can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), greater<int>());
+        if(arr.size()==1){
+            return true;
+        }
+        int diff = arr[0]-arr[1];
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i]-arr[i+1] != diff){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
