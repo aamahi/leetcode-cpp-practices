082d5cc63cc2d4f718c44e039ada94044e4872ea
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-10-09 22:11:25 -0700

    Time: 61 ms (97.27%), Space: 41.9 MB (68.18%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.js
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -2,11 +2,17 @@
  * @param {string} palindrome
  * @return {string}
  */
-var breakPalindrome = function (p) {
+var breakPalindrome = function (palindrome) {
+  let p = palindrome
   if (p.length === 1) return ""
 
-  for (let i = 0; i < Math.floor(p.length / 2); i++) {
-    if (p.charAt(i) != "a") return p.substring(0, i) + "a" + p.substring(i + 1, p.length)
+  let mid = Math.floor(p.length / 2)
+
+  for (let i = 0; i < mid; i++) {
+    let char = p.charAt(i)
+    if (char != "a") {
+      return p.substring(0, i) + "a" + p.substring(i + 1, p.length)
+    }
   }
   return p.substring(0, p.length - 1) + "b"
 };
