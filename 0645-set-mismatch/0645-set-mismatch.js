207ab5e37d1c9d2305e229f1b4e65ab725c9b053
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-10-22 22:48:30 -0700

    Time: 78 ms (96.90%), Space: 46.4 MB (53.23%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    
+};
+
+var findErrorNums = function(nums) {
+    const obj = {};
+    
+    let repeated;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]]) repeated = [nums[i]];
+        obj[nums[i]] = nums[i];
+    };
+    for (let i = 1; i <= nums.length; i++) {
+        if (!obj[i]) return [repeated, i];
+    };
+};
