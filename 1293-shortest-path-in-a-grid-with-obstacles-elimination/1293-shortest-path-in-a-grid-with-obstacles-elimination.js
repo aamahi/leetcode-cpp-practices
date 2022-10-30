29bbeae139847b0b1ce14347177e562d18c395a6
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-10-30 08:22:34 -0700

    Time: 614 ms (59.21%), Space: 64.6 MB (78.51%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -1,20 +1,51 @@
 var shortestPath = function(grid, k) {
-    let visited = new Set()
-    let queue = [[0,0,k,0]]//curx,cury,eliminateT,step
-    let dirs = [[0,1],[1,0],[-1,0],[0,-1]]
-    while(queue.length){
-        let [curx,cury,eleminateT,step] = queue.shift()
-        if (visited.has(`${curx}${cury}${eleminateT}`)) continue
-        if (eleminateT === -1) continue
-        if (curx === grid.length - 1 && cury === grid[0].length - 1) return step 
-        visited.add(`${curx}${cury}${eleminateT}`)
-        for (let [dx,dy] of dirs) {
-            let nextX = curx + dx
-            let nextY = cury + dy
-            if (nextX < 0 || nextX >= grid.length || nextY < 0 || nextY >= grid[0].length) continue
-            if (grid[nextX][nextY] === 0) queue.push([nextX, nextY, eleminateT,step + 1] )
-            else queue.push([nextX, nextY, eleminateT - 1,step + 1] )
+    // Constants
+    const DIRECTIONS = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // [Up, Right, Down, Left];
+    const m = grid.length;
+    const n = grid[0].length;
+    
+    let steps = 0;
+    const visited = new Set();
+    let queue = [[0,0,k]];
+    
+    // BFS O(V + E) = O(M * N + 4(M * N)) = O(MN)
+    while (queue.length) {
+        let nextQueue = []; 
+        while (queue.length) {
+            // Start polling items from the queue
+            const [i, j, kLeft] = queue.shift();    
+            
+            // If the cell has been visited before, skip
+            if (visited.has(`${i},${j},${kLeft}`)) {
+                continue;
+            } else {
+                visited.add(`${i},${j},${kLeft}`);
+            }
+            
+            // Has reached the end
+            if (i === m - 1 && j === n - 1) { 
+                return steps;
+            }
+            
+            // Try to move in all directions
+            for (const direction of DIRECTIONS) {
+                const nextI = i + direction[0];
+                const nextJ = j + direction[1];
+                
+                // If the coordinates are out of bounds OR a wall has been encountered with no more k left, skip 
+                if (nextI < 0 || nextJ < 0 || nextI >= m || nextJ >= n || grid[nextI][nextJ] && !kLeft) {
+                    continue;
+                }
+                
+                // Add the next step to the next iteration of BFS
+                const nextK = grid[nextI][nextJ] ? kLeft - 1 : kLeft;
+                nextQueue.push([nextI, nextJ, nextK]);
+            }
         }
+        queue = nextQueue;
+        steps++;
     }
-    return -1
+    
+      
+    return -1;
 };
