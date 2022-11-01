905e9543a9fefb17fa94da827f1cd421b1766aee
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-11-01 06:07:22 -0700

    Time: 125 ms (30.45%), Space: 44.7 MB (37.82%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -1,46 +1,27 @@
-/**
- * @param {number[][]} grid
- * @return {number[]}
- */
 var findBall = function(grid) {
-    let balls = grid[0].length;
-    const ans = new Array(balls).fill(-1);
-    for (let i = 0; i < ans.length; i++) {
-        ans[i] = dfs(grid, 0, i);
+    const rows = grid.length;
+    const cols = grid[0].length;
+    
+    function findPath(col) {
+        let row = 0;
+        while (row < rows) {
+            if ((col == 0 && grid[row][col] == -1) ||
+                (col == cols-1 && grid[row][col] == 1) ||
+                (grid[row][col] == 1 && col < cols-1 && grid[row][col+1] == -1) ||
+                (grid[row][col] == -1 && col > 0 && grid[row][col-1] == 1)) {
+                // stuck
+                return -1;
+            }
+            
+            if (grid[row][col] == 1) {
+                col++;
+            } else {
+                col--;
+            }
+            row++
+        }
+        return col;
     }
-    return ans;
+    
+    return Array.from(Array(cols).keys()).map(col => findPath(col));
 };
-
-var dfs = function(grid, r, c) {
-    // is stuck in a V
-    if (
-        grid[r][c] === 1 && grid[r][c+1] === -1 ||
-        grid[r][c] === -1 && grid[r][c-1] === 1
-    ) {
-        return -1;
-    }
-    
-    // is stuck on left wall
-    if (grid[r][c] === -1 && c - 1 < 0) {
-        return -1;
-    }
-    // is stuck on right wall
-    if (grid[r][c] === 1 && c + 1 >= grid[0].length) {
-        return -1;
-    }
-    
-    // when ball drops to bottom :)
-    if (r === grid.length - 1) {
-        // drops to right
-        if (grid[r][c] === 1) return c + 1;
-        // drops to left
-        return c - 1;
-    }
-    
-    // dfs right
-    if (grid[r][c] === 1) {
-        return dfs(grid, r + 1, c + 1);
-    }
-    //  dfs left
-    return dfs(grid, r + 1, c - 1);
-}
