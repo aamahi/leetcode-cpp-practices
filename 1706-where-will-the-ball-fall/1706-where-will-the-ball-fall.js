96730f66bebca5171aa75155bb69cf73c8c42c0e
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-11-01 06:09:15 -0700

    Time: 128 ms (25.48%), Space: 44 MB (94.07%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -1,32 +1,28 @@
 var findBall = function(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
+    let n = grid.length;
+    if( n == 0 ) return [];
+    let m = grid[0].length;
+    var i, x, y;
+    let spots = new Array();
+    var dist;
     
-    const res = [];
-        
-    for (let j = 0; j < n; j++) {
-        res[j] = j;
+    for( i=0; i<m; i++ ) {
+        spots.push(i);
     }
     
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (res[j] === -1) continue;
-            
-            const currCol = res[j];
-            
-            if (currCol < n - 1 && grid[i][currCol] === 1 && grid[i][currCol + 1] === -1) {
-                res[j] = -1;
-            }
-            else if (currCol > 0 && grid[i][currCol] === -1 && grid[i][currCol - 1] === 1) {
-                res[j] = -1;
-            }
-            else {
-                res[j] += grid[i][currCol];
-               
-                if (res[j] === n) res[j] = -1;
+    for( y=0; y<n; y++ ) {
+        for( i in spots ) {
+            x = spots[i];
+            if( x == -1 ) continue;
+            dist = grid[y][x];
+            x += dist;
+            if( x>=m || x<0 || grid[y][x] == -dist ) {
+                spots[i] = -1;
+            } else {
+                spots[i] = x;
             }
         }
     }
     
-    return res;
+    return spots;
 };
