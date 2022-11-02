650f9df73a287101234d4e54fe4352be14a9af1c
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-11-02 06:04:34 -0700

    Time: 104 ms (28.81%), Space: 41.9 MB (67.80%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -1,24 +1,31 @@
 var minMutation = function(start, end, bank) {
-    let tempBanks = new Set(bank);  
-    let strChars = ['A', 'T', 'C', 'G'];        
-    let Q = [[start,0]];
-    let item, dist, i, j;
-    if( !tempBanks.has(end) ) return -1;
-    
-    while( Q.length > 0 ) {
-        [item,dist] = Q.shift();
-        if( item == end ) return dist;
-        
-        for( i=0; i<8; i++ ) {
-            for( j=0; j<4; j++ ) {
-                if( item[i] == strChars[j] ) continue;
-                var node = item.slice(0,i) + strChars[j] + item.slice(i+1);
-                if( tempBanks.has(node) ) {
-                    Q.push([node,dist+1]);
-                    tempBanks.delete(node);
+    const options = ['A', 'C', 'G', 'T'];
+    let set = new Set(bank);
+    if (!set.has(end)) return -1;
+    let q = [start];
+    let steps = 0;
+    while (q.length) {
+        let len = q.length;
+        for (let i = 0; i < len; i++) {
+            let dna = q.shift();
+            // try all one step moves
+			for (let j = 0; j < 8; j++) {
+                for (let option of options) {
+                    const newDNA = dna.slice(0, j) + option + dna.slice(j + 1);
+                    if (newDNA === end) {
+                        return steps + 1;
+                    };
+                    
+                    if (set.has(newDNA)) {
+                        set.delete(dna);
+                        q.push(newDNA);
+                    }
                 }
             }
         }
-    }   
+        
+        steps++;
+    }
+    
     return -1;
 };
